eb2fe8e507dab7e8182de3dc90b0ee05e2d3db54
Roee Furman <roeefurman1@gmail.com>
2022-06-23 09:20:38 +0300

    FIX: search value to lower case

--- src/services/albums.service.js
+++ src/services/albums.service.js
@@ -8,10 +8,11 @@ export const albumsService = {
 }
 
 async function query(value) {
+    const valueToLower = value.toLocaleLowerCase()
     const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`)
     if (value === '' || !value) return res.data
     return res.data.filter((album) =>
-        album.title.toLocaleLowerCase().includes(value)
+        album.title.toLocaleLowerCase().includes(valueToLower)
     )
 }
 
